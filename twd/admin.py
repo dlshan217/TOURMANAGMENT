2ed0c0ae34da78eecbdedb14761b80a9dac754ac
dlshan217 <mohammaddilshan217@gmail.com>
2025-04-24 20:42:13 +0530

    TOUR MANAGMENT

---
+++ twd/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Usersign_up)
+admin.site.register(Vendorregister)
+admin.site.register(Bookingdetails)
+admin.site.register(Packagecreate)
+
